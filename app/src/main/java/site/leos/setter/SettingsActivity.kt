6aea357660836625298597e50ee7989cd0b81c40
Jeffrey Liu <scubajeffrey@gmail.com>
2020-06-28 11:26:47 +0800

    install ColorDict link in setting screen

--- app/src/main/java/site/leos/setter/SettingsActivity.kt
+++ app/src/main/java/site/leos/setter/SettingsActivity.kt
@@ -1,11 +1,10 @@
 package site.leos.setter
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
 
@@ -27,35 +26,14 @@ class SettingsActivity : AppCompatActivity() {
 
         override fun onResume() {
             super.onResume()
-            if (isColorDictAvailable()) {
-                // Enable ColorDict setter activity
-                //context!!.packageManager.setComponentEnabledSetting(
-                //    ComponentName(context!!, site.leos.setter.DictActivity::class.java), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP)
 
-                findPreference<SwitchPreferenceCompat>(getString(R.string.colordict_fullscreen_key))?.apply {
-                    isEnabled = true
-                    summary = ""
-                }
-            } else {
-                findPreference<SwitchPreferenceCompat>(getString(R.string.colordict_fullscreen_key))?.let {
-                    it.summary = getString(R.string.colordict_not_installed)
-                    it.setOnPreferenceClickListener {
-                        try {startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://$COLORDICT_PACKAGE_NAME")))
-                        } catch (e: ActivityNotFoundException) {
-                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/$COLORDICT_PACKAGE_NAME")))
-                        }
-                        true
-                    }
-                }
-            }
+            val colorDictAvailable = isColorDictAvailable()
+            findPreference<SwitchPreferenceCompat>(getString(R.string.colordict_fullscreen_key))?.isVisible = colorDictAvailable
+            findPreference<Preference>(getString(R.string.install_colordict_key))?.isVisible = !colorDictAvailable
         }
 
         private fun isColorDictAvailable(): Boolean {
             return requireActivity().packageManager?.queryIntentActivities(Intent("colordict.intent.action.SEARCH"), PackageManager.MATCH_DEFAULT_ONLY)?.size!! > 0
-        }
-
-        companion object {
-            const val COLORDICT_PACKAGE_NAME = "details?id=com.socialnmobile.colordict"
         }
     }
 }
